9daa5bf38c3e8063db2fc5e6324f33dada6878ad
xcesco <xcesco@gmail.com>
2019-12-13 01:10:50 +0100

    @wip

--- app/src/main/java/it/fmt/games/reversi/android/GameActivity.java
+++ app/src/main/java/it/fmt/games/reversi/android/GameActivity.java
@@ -2,6 +2,7 @@ package it.fmt.games.reversi.android;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +11,30 @@ import androidx.appcompat.widget.Toolbar;
 import java.text.MessageFormat;
 import java.util.List;
 
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import it.fmt.games.reversi.GameRenderer;
 import it.fmt.games.reversi.PlayerFactory;
 import it.fmt.games.reversi.Reversi;
 import it.fmt.games.reversi.UserInputReader;
+import it.fmt.games.reversi.android.ui.AppGridLayout;
+import it.fmt.games.reversi.android.ui.GridViewItem;
+import it.fmt.games.reversi.model.Board;
 import it.fmt.games.reversi.model.Coordinates;
 import it.fmt.games.reversi.model.GameSnapshot;
 import it.fmt.games.reversi.model.Player;
 
 
-public class GameActivity extends AppCompatActivity implements GameRenderer, UserInputReader {
+public class GameActivity extends AppCompatActivity implements GameRenderer, UserInputReader, View.OnClickListener {
 
     @BindView(R.id.blackNum)
     TextView blackNum;
 
     @BindView(R.id.whiteNum)
     TextView whiteNum;
+
+    @BindView(R.id.gridLayout)
+    AppGridLayout appGridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +46,7 @@ public class GameActivity extends AppCompatActivity implements GameRenderer, Use
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Reversi reversi = new Reversi(this, this, PlayerFactory.createRoboPlayer1(), PlayerFactory.createRoboPlayer2());
-
+        Reversi reversi = new Reversi(this, this, PlayerFactory.createUserPlayer1(), PlayerFactory.createRoboPlayer2());
         reversi.play();
     }
 
@@ -52,13 +57,33 @@ public class GameActivity extends AppCompatActivity implements GameRenderer, Use
         this.whiteNum.setText(MessageFormat.format("WHITE: {0}", gameSnapshot.getScore().getPlayer2Score()));
 
         gameSnapshot.getBoard().getCellStream().forEach(item -> {
-            //item.get
+            GridViewItem view= (GridViewItem) appGridLayout.getChildAt(item.getCoordinates().getRow()* Board.BOARD_SIZE+item.getCoordinates().getColumn());
+            view.setOnClickListener(GameActivity.this);
+            view.setTag(item.getCoordinates());
+            switch(item.getPiece()) {
+                case EMPTY:
+                    view.setImageResource(R.drawable.transparent);
+                    break;
+                case PLAYER_1:
+                    view.setImageResource(R.drawable.black_256);
+                    break;
+                case PLAYER_2:
+                    view.setImageResource(R.drawable.white_256);
+                    break;
+            }
         });
+
     }
 
 
     @Override
     public Coordinates readInputFor(Player player, List<Coordinates> list) {
         return list.get(0);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Coordinates coordinate= (Coordinates) v.getTag();
+
     }
 }
